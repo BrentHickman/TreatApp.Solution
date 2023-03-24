e51709dbabe822a016415c868d3bd73f9fe54ed3
BrentHickman <brenthickman@ymail.com>
2023-03-24 13:10:21 -0700

    wip add Price to Treat model. update migration.

--- TreatApp/Controllers/AccountController.cs
+++ TreatApp/Controllers/AccountController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Net.Mail;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using TreatApp.Models;
--- TreatApp/Migrations/20230324200927_AddPricePropTreat.Designer.cs
+++ TreatApp/Migrations/20230324200927_AddPricePropTreat.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TreatApp.Models;
 
@@ -10,9 +11,10 @@ using TreatApp.Models;
 namespace TreatApp.Migrations
 {
     [DbContext(typeof(TreatAppContext))]
-    partial class TreatAppContextModelSnapshot : ModelSnapshot
+    [Migration("20230324200927_AddPricePropTreat")]
+    partial class AddPricePropTreat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
